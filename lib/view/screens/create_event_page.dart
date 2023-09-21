dd806eed85db2433f17d7cc54006fe9f5e881c06
gwave <adebayooluwatobi2018@gmail.com>
2023-09-21 04:05:07 +0100

    completed. inly the flytter pick time package left

--- lib/view/screens/create_event_page.dart
+++ lib/view/screens/create_event_page.dart
@@ -2,41 +2,49 @@ import 'package:event_app/controller/tab_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
 
 import '../../util/color_lib.dart';
 // import '../../util/fonts.dart';
 // import '../widgets/stroke_text.dart';
 
-class CreateEventPage extends ConsumerWidget {
+
+class CreateEventPage extends StatefulWidget {
   const CreateEventPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  State<CreateEventPage> createState() => _CreateEventPageState();
+}
+
+class _CreateEventPageState extends State<CreateEventPage> {
+  DateTime startDate = DateTime.now();
+  DateTime endDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Create Event'),
         leading: IconButton(
-          onPressed: () =>
-              ref.read(tabProvider.notifier).state = TabState.timeline,
+          onPressed: () => Navigator.pop(context),
+          // ref.read(tabProvider.notifier).state = TabState.timeline,
           icon: SvgPicture.asset('assets/SVGs/back-button.svg'),
         ),
       ),
       backgroundColor: ColorLib.transparent,
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(24.0),
+          padding: const EdgeInsets.all(24),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const CustomText(
                 text: 'Event Name',
               ),
-              const SizedBox(
-                height: 15,
-              ),
+              const SizedBox(height: 15),
               Container(
-               width: double.infinity,
+                width: double.infinity,
                 height: 63,
                 decoration: ShapeDecoration(
                   color: const Color(0xFFF5FAFF),
@@ -64,9 +72,7 @@ class CreateEventPage extends ConsumerWidget {
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
+              const SizedBox(height: 20),
               const CustomText(
                 text: 'Event Description',
               ),
@@ -74,7 +80,7 @@ class CreateEventPage extends ConsumerWidget {
                 height: 15,
               ),
               Container(
-               width: double.infinity,
+                width: double.infinity,
                 height: 173,
                 decoration: ShapeDecoration(
                   color: const Color(0xFFF5FAFF),
@@ -106,46 +112,42 @@ class CreateEventPage extends ConsumerWidget {
               const CustomText(
                 text: 'When would the event start?',
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Row(
+              const SizedBox(height: 20),
+              Row(
                 children: [
-                  Dropdown_Cell(
-                    text: '07 May, 2023',
+                  DropdownCell(
+                    selectedDate: startDate,
+                    onDateChanged: (date) => setState(() => startDate = date),
                   ),
-                  SizedBox(
-                    width: 24,
+                  const SizedBox(width: 24),
+                  //                  TimePickerWidget(), // TODO: Add time picker widget here!
+                  DropdownCell(
+                    selectedDate: startDate,
+                    onDateChanged: (date) => setState(() => startDate = date),
                   ),
-                  Dropdown_Cell(
-                    text: '13:00',
-                  )
                 ],
               ),
-              const SizedBox(
-                height: 20,
-              ),
+              const SizedBox(height: 20),
               const CustomText(
                 text: 'When would the event end?',
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Row(
+              const SizedBox(height: 20),
+              Row(
                 children: [
-                  Dropdown_Cell(
-                    text: '07 May, 2023',
+                  DropdownCell(
+                    selectedDate: endDate,
+                    onDateChanged: (date) => setState(() => endDate = date),
                   ),
-                  SizedBox(
-                    width: 24,
+                  const SizedBox(width: 24),
+                  //                  TimePickerWidget(), // TODO: Add time picker widget here!
+                  DropdownCell(
+                    selectedDate: endDate,
+                    onDateChanged: (date) => setState(() => endDate = date),
                   ),
-                  Dropdown_Cell(
-                    text: '18:00',
-                  )
                 ],
               ),
               const SizedBox(
-                height: 20,
+                height: 15,
               ),
               SizedBox(
                 width: 131,
@@ -232,7 +234,7 @@ class CreateEventPage extends ConsumerWidget {
                 height: 15,
               ),
               Container(
-                width: 375,
+                width: double.infinity,
                 height: 64,
                 padding:
                     const EdgeInsets.symmetric(horizontal: 71, vertical: 18),
@@ -290,35 +292,21 @@ class CreateEventPage extends ConsumerWidget {
   }
 }
 
-class CustomText extends StatelessWidget {
-  final String text;
-  const CustomText({Key? key, required this.text}) : super(key: key);
+class DropdownCell extends StatefulWidget {
+  final DateTime selectedDate;
+  final ValueChanged<DateTime> onDateChanged;
+
+  const DropdownCell({
+    super.key,
+    required this.selectedDate,
+    required this.onDateChanged,
+  });
 
   @override
-  Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: const TextStyle(
-        color: Colors.black,
-        fontSize: 16,
-        fontFamily: 'Tropiline',
-        fontWeight: FontWeight.w600,
-        height: 0.09,
-        letterSpacing: 0.16,
-      ),
-    );
-  }
+  State<DropdownCell> createState() => _DropdownCellState();
 }
 
-// ignore: camel_case_types
-class Dropdown_Cell extends StatelessWidget {
-  final String text; // Add a String parameter for the text content
-
-  const Dropdown_Cell({
-    Key? key,
-    required this.text, // Require the text parameter
-  }) : super(key: key);
-
+class _DropdownCellState extends State<DropdownCell> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -341,10 +329,12 @@ class Dropdown_Cell extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                text,
-                style: cell_text_style(),
+                DateFormat('MM/dd/yyyy').format(widget.selectedDate),
               ),
-             const Icon(Icons.arrow_drop_down),
+              IconButton(
+                icon: const Icon(Icons.arrow_drop_down),
+                onPressed: _showDatePicker,
+              ),
             ],
           ),
         ),
@@ -352,14 +342,392 @@ class Dropdown_Cell extends StatelessWidget {
     );
   }
 
-  // ignore: non_constant_identifier_names
-  TextStyle cell_text_style() {
-    return const TextStyle(
-      color: Colors.black,
-      fontSize: 16,
-      fontFamily: 'Nunito',
-      fontWeight: FontWeight.w500,
-      height: 0.06,
+  _showDatePicker() async {
+    final DateTime? date = await showDatePicker(
+      context: context,
+      initialDate: widget.selectedDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2025),
+    );
+
+    if (date != null) {
+      widget.onDateChanged(date);
+    }
+  }
+}
+
+class CustomText extends StatelessWidget {
+  final String text;
+  const CustomText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        color: Colors.black,
+        fontSize: 16,
+        fontFamily: 'Tropiline',
+        fontWeight: FontWeight.w600,
+        height: 0.09,
+        letterSpacing: 0.16,
+      ),
     );
   }
 }
+// class CreateEventPage extends ConsumerWidget {
+//   const CreateEventPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         centerTitle: true,
+//         title: const Text('Create Event'),
+//         leading: IconButton(
+//           onPressed: () =>
+//               ref.read(tabProvider.notifier).state = TabState.timeline,
+//           icon: SvgPicture.asset('assets/SVGs/back-button.svg'),
+//         ),
+//       ),
+//       backgroundColor: ColorLib.transparent,
+//       body: SingleChildScrollView(
+//         child: Padding(
+//           padding: const EdgeInsets.all(24.0),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               const CustomText(
+//                 text: 'Event Name',
+//               ),
+//               const SizedBox(
+//                 height: 15,
+//               ),
+// Container(
+//  width: double.infinity,
+//   height: 63,
+//   decoration: ShapeDecoration(
+//     color: const Color(0xFFF5FAFF),
+//     shape: RoundedRectangleBorder(
+//       side: const BorderSide(
+//         width: 2,
+//         strokeAlign: BorderSide.strokeAlignCenter,
+//       ),
+//       borderRadius: BorderRadius.circular(8),
+//     ),
+//   ),
+//   child: Padding(
+//     padding: const EdgeInsets.only(
+//       left: 16,
+//     ),
+//     child: Center(
+//       child: TextFormField(
+//         maxLines: 2,
+//         autocorrect: true,
+//         decoration: const InputDecoration(
+//           hintText: 'Type Event Name',
+//           border: InputBorder.none,
+//         ),
+//       ),
+//     ),
+//   ),
+// ),
+// const SizedBox(
+//   height: 20,
+// ),
+// const CustomText(
+//   text: 'Event Description',
+// ),
+// const SizedBox(
+//   height: 15,
+// ),
+// Container(
+//  width: double.infinity,
+//   height: 173,
+//   decoration: ShapeDecoration(
+//     color: const Color(0xFFF5FAFF),
+//     shape: RoundedRectangleBorder(
+//       side: const BorderSide(
+//         width: 2,
+//         strokeAlign: BorderSide.strokeAlignCenter,
+//       ),
+//       borderRadius: BorderRadius.circular(8),
+//     ),
+//   ),
+//   child: Padding(
+//     padding: const EdgeInsets.only(
+//       left: 16,
+//     ),
+//     child: TextFormField(
+//       decoration: const InputDecoration(
+//         hintText: 'Type event description',
+//         border: InputBorder.none,
+//       ),
+//       autocorrect: true,
+//       maxLines: 20,
+//     ),
+//   ),
+// ),
+//               const SizedBox(
+//                 height: 20,
+//               ),
+//               const CustomText(
+//                 text: 'When would the event start?',
+//               ),
+//               const SizedBox(
+//                 height: 20,
+//               ),
+//               const Row(
+//                 children: [
+//                   Dropdown_Cell(
+//                     text: '07 May, 2023',
+//                   ),
+//                   SizedBox(
+//                     width: 24,
+//                   ),
+//                   Dropdown_Cell(
+//                     text: '13:00',
+//                   )
+//                 ],
+//               ),
+//               const SizedBox(
+//                 height: 20,
+//               ),
+//               const CustomText(
+//                 text: 'When would the event end?',
+//               ),
+//               const SizedBox(
+//                 height: 20,
+//               ),
+//               const Row(
+//                 children: [
+//                   Dropdown_Cell(
+//                     text: '07 May, 2023',
+//                   ),
+//                   SizedBox(
+//                     width: 24,
+//                   ),
+//                   Dropdown_Cell(
+//                     text: '18:00',
+//                   )
+//                 ],
+//               ),
+//               const SizedBox(
+//                 height: 20,
+//               ),
+// SizedBox(
+//   width: 131,
+//   height: 24,
+//   child: Row(
+//     mainAxisSize: MainAxisSize.min,
+//     mainAxisAlignment: MainAxisAlignment.center,
+//     crossAxisAlignment: CrossAxisAlignment.center,
+//     children: [
+//       Container(
+//         width: 24,
+//         height: 24,
+//         clipBehavior: Clip.antiAlias,
+//         decoration: const BoxDecoration(),
+//         child: const Stack(
+//           children: [
+//             Icon(
+//               Icons.location_on,
+//               size: 20.0,
+//               color: Color.fromRGBO(
+//                   195, 155, 233, 1), // Set the color
+//             ),
+//           ],
+//         ),
+//       ),
+//       const SizedBox(width: 8),
+//       const Text(
+//         'Add location',
+//         style: TextStyle(
+//           color: Colors.black,
+//           fontSize: 16,
+//           fontFamily: 'Tropiline',
+//           fontWeight: FontWeight.w700,
+//           height: 0.09,
+//           letterSpacing: 0.16,
+//         ),
+//       ),
+//     ],
+//   ),
+// ),
+//               const SizedBox(
+//                 height: 15,
+//               ),
+// SizedBox(
+//   width: 153,
+//   height: 32,
+//   child: Row(
+//     mainAxisSize: MainAxisSize.min,
+//     mainAxisAlignment: MainAxisAlignment.center,
+//     crossAxisAlignment: CrossAxisAlignment.center,
+//     children: [
+//       Container(
+//         width: 32,
+//         height: 32,
+//         clipBehavior: Clip.antiAlias,
+//         decoration: const BoxDecoration(),
+//         child: const Stack(
+//           children: [
+//             Icon(
+//               Icons.group,
+//               size: 20.0,
+//               color: Color.fromRGBO(
+//                   195, 155, 233, 1), // Set the color
+//             ),
+//           ],
+//         ),
+//       ),
+//       const SizedBox(width: 8),
+//       const Text(
+//         'Select Groups',
+//         style: TextStyle(
+//           color: Colors.black,
+//           fontSize: 16,
+//           fontFamily: 'Tropiline',
+//           fontWeight: FontWeight.w700,
+//           height: 0.09,
+//           letterSpacing: 0.16,
+//         ),
+//       ),
+//     ],
+//   ),
+// ),
+//               const SizedBox(
+//                 height: 15,
+//               ),
+// Container(
+//   width: 375,
+//   height: 64,
+//   padding:
+//       const EdgeInsets.symmetric(horizontal: 71, vertical: 18),
+//   clipBehavior: Clip.antiAlias,
+//   decoration: ShapeDecoration(
+//     color: const Color(0xFFDEEDF7),
+//     shape: RoundedRectangleBorder(
+//       side: const BorderSide(
+//         width: 2,
+//         strokeAlign: BorderSide.strokeAlignCenter,
+//       ),
+//       borderRadius: BorderRadius.circular(8),
+//     ),
+//     shadows: const [
+//       BoxShadow(
+//         color: Color(0xFF000000),
+//         blurRadius: 0,
+//         offset: Offset(4, 4),
+//         spreadRadius: 0,
+//       )
+//     ],
+//   ),
+//   child: const Row(
+//     mainAxisSize: MainAxisSize.min,
+//     mainAxisAlignment: MainAxisAlignment.center,
+//     crossAxisAlignment: CrossAxisAlignment.center,
+//     children: [
+//       Flexible(
+//         flex: 1,
+//         fit: FlexFit.loose,
+//         child: SizedBox(
+//           width: 233,
+//           child: Text(
+//             'Create Event',
+//             textAlign: TextAlign.center,
+//             style: TextStyle(
+//               color: Colors.black,
+//               fontSize: 20,
+//               fontFamily: 'Cabinet Grotesk',
+//               fontWeight: FontWeight.w700,
+//               height: 0.07,
+//               letterSpacing: 0.20,
+//             ),
+//           ),
+//         ),
+//       ),
+//     ],
+//   ),
+// )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class CustomText extends StatelessWidget {
+//   final String text;
+//   const CustomText({Key? key, required this.text}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text(
+//       text,
+//       style: const TextStyle(
+//         color: Colors.black,
+//         fontSize: 16,
+//         fontFamily: 'Tropiline',
+//         fontWeight: FontWeight.w600,
+//         height: 0.09,
+//         letterSpacing: 0.16,
+//       ),
+//     );
+//   }
+// }
+
+// // ignore: camel_case_types
+// class Dropdown_Cell extends StatelessWidget {
+//   final String text; // Add a String parameter for the text content
+
+//   const Dropdown_Cell({
+//     Key? key,
+//     required this.text, // Require the text parameter
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Expanded(
+//       child: Container(
+//         width: 178,
+//         height: 63,
+//         decoration: ShapeDecoration(
+//           color: const Color(0xFFF5FAFF),
+//           shape: RoundedRectangleBorder(
+//             side: const BorderSide(
+//               width: 2,
+//               strokeAlign: BorderSide.strokeAlignCenter,
+//             ),
+//             borderRadius: BorderRadius.circular(8),
+//           ),
+//         ),
+//         child: Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             children: [
+//               Text(
+//                 text,
+//                 style: cell_text_style(),
+//               ),
+//              const Icon(Icons.arrow_drop_down),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   // ignore: non_constant_identifier_names
+//   TextStyle cell_text_style() {
+//     return const TextStyle(
+//       color: Colors.black,
+//       fontSize: 16,
+//       fontFamily: 'Nunito',
+//       fontWeight: FontWeight.w500,
+//       height: 0.06,
+//     );
+//   }
+// }
