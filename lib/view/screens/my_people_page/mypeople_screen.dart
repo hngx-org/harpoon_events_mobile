2d8a459973dad9cdf4443be63dfb2b17a4fdae71
Papa Kofi <humblepapa1234@gmail.com>
2023-09-21 21:10:47 +0000

    Added appropriate navigations

--- lib/view/screens/my_people_page/mypeople_screen.dart
+++ lib/view/screens/my_people_page/mypeople_screen.dart
@@ -35,7 +35,7 @@ class _MyPeopleState extends State<MyPeople> {
           ),
         ),
         body: SingleChildScrollView(
-            child: Container(
+            child: SizedBox(
                 height: size.height,
                 width: size.width,
                 // decoration: const BoxDecoration(color: Color(0xFFF5FAFF)),
@@ -44,14 +44,18 @@ class _MyPeopleState extends State<MyPeople> {
                     SizedBox(
                       height: size.height * 0.04,
                     ),
-                    const Opacity(
+                    Opacity(
                       opacity: 0.50,
                       child: Padding(
-                        padding: EdgeInsets.all(8.0),
+                        padding: EdgeInsets.only(left: size.width * 0.05),
                         child: Text(
                           'Keep track of events your connections are attending or hosting.',
                           softWrap: true,
-                          style: TextStyle(color: Colors.black, fontSize: 17),
+                          style: Fonts.nunito(
+                            color: ColorLib.grey,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
                     ),
